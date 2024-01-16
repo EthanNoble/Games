4bcc5cfeb265700e340ed28b93ca304aba84802a
EthanNoble <enoble1@uco.edu>
2024-01-16 01:28:01 -0600

    Much better tetris code base

---
+++ Tetris2/game_objects.py
@@ -0,0 +1,47 @@
+import pygame as pg
+from variables import colors as c
+from variables import tetrominos
+from random import choice
+
+
+class Tetromino: # Collection of block sprites, not a sprite itself
+    def __init__(self, block_size, x, y, alpha=255, border_width=0):
+        # Random tetromino color
+        tetromino_colors = [c['cyan'], c['orange'], c['light green'], c['yellow'], c['pink']]
+        color = choice(tetromino_colors)
+
+        # Create 4 blocks for tetromino
+        blocks = [Block(block_size, x, y, color, alpha, border_width) for _ in range(4)]
+        self.block_group = pg.sprite.RenderUpdates(blocks)
+
+        # Select random tetromino type
+        self.__type = choice(list(tetrominos.values()))
+        self.__position_blocks()
+    
+    def __position_blocks(self): # Position blocks according to tetromino type
+        relative_positions = [] # Relative positions of blocks in tetromino
+        for y in range(len(self.__type)):
+            for x in range(len(self.__type[y])):
+                if self.__type[y][x] is 1:
+                    relative_positions.append((x, y))
+
+        # Add relative positions to the blocks absolute x and y positions
+        for i, block in enumerate(self.block_group.sprites()):
+            block.rect.x += relative_positions[i][0] * block.rect.width
+            block.rect.y += relative_positions[i][1] * block.rect.height
+
+
+
+
+class Block(pg.sprite.Sprite):
+    def __init__(self, size, x, y, color, alpha=255, border_width=0): # border_width = 0 -> no border & filled block
+        pg.sprite.Sprite.__init__(self)
+
+        self.image = pg.Surface([size, size])
+        self.image.set_alpha(alpha)
+        self.image.fill(color)
+
+        # Set the values of rect.x and rect.y to update block's position
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
---
+++ Tetris2/main.py
@@ -0,0 +1,72 @@
+import pygame as pg
+from game_objects import Block
+from game_objects import Tetromino
+from variables import colors as c
+
+
+WIDTH = 650
+HEIGHT = 660
+GRID_WIDTH = 12
+GRID_HEIGHT = 22
+CELL_SIZE = 30
+
+
+def borders():
+    group = pg.sprite.RenderUpdates()
+    for y in range(GRID_HEIGHT):
+        for x in range(GRID_WIDTH):
+            if (x is not 0 and
+                x is not GRID_WIDTH-1 and
+                y is not 0 and
+                y is not GRID_HEIGHT-1):
+                continue # Skip if not edge of grid
+            block = Block(CELL_SIZE, x*CELL_SIZE, y*CELL_SIZE, c['grey'])
+            group.add(block)
+    return group
+
+
+def main():
+    pg.init()
+    screen = pg.display.set_mode((WIDTH, HEIGHT), pg.SCALED)
+    pg.display.set_caption('Tetris')
+    pg.mouse.set_visible(True)
+
+    background = pg.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((c['black']))
+
+    screen.blit(background, (0, 0))
+    pg.display.flip()
+
+    # Game objects including sprites and groups
+    border_group = borders()
+    falling_x = (GRID_WIDTH//2 - 2) * CELL_SIZE
+    falling_y = 1 * CELL_SIZE
+    falling_tetromino = Tetromino(CELL_SIZE, falling_x, falling_y)
+    clock = pg.time.Clock()
+
+    running = True
+    while running:
+        clock.tick(60)
+
+        # Input queue
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+        
+        # UPDATE ALL SPRITES HERE
+        border_group.update()
+        falling_tetromino.block_group.update()
+                
+        screen.blit(background, (0, 0))
+        # DRAW ALL SPRITES HERE
+        border_group.draw(screen)
+        falling_tetromino.block_group.draw(screen)
+
+        pg.display.flip()
+    
+    pg.quit()
+
+
+if __name__ == '__main__':
+    main()
